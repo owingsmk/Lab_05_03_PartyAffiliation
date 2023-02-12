c130652463e2954531ad9bebaf1c571013e448d6
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 16:27:22 -0500

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("What is your party? Use: R, D, or I: ");
-         String partyAffiliation = "R";
+         String partyAffiliation = "D";
 
-         if (partyAffiliation == "R");{
+         if (partyAffiliation.equals("R")){
              System.out.println("You get a Republican Elephant");
-        }if (partyAffiliation == "D");{
-            System.out.println("You get a Democrat Donkey!");
-        }if (partyAffiliation == "I");{
-            System.out.println("You get a Independant Man!");
-        }
+         }
+         if (partyAffiliation.equals("D")){
+             System.out.println("You get a Democrat Donkey!");
+         }
+         if (partyAffiliation.equals("I")){
+             System.out.println("You get a Independant Man!");
+         }
 
     }
 }
